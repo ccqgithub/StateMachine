ab68d6c98fce79998a78df89494dd0e493f043e9
season.chen <563721966@qq.com>
2015-08-27 18:12:33 +0800

    'v1'

--- README.md
+++ README.md
@@ -1,2 +1,221 @@
-# StateMachine
-Javascript 有限状态机
+StateMachine: Javascript 有限状态机
+==
+
+## 什么是有限状态机？
+
+> 有限状态机，（英语：Finite-state machine, FSM），又称有限状态自动机，简称状态机，是表示有限个状态以及在这些状态之间的转移和动作等行为的数学模型。
+
+更详细的信息可以自行百度。
+
+## 为什么要用状态机？
+
+世间的一切，都可以看成是各种状态的集合。比如一块石头，今天可能是干的，明天可能是湿的。再比如一个人，年龄会变，可以从结婚变成未结婚，可以从男人变成女人，等等等……
+
+而状态改变的时候，我们可能要做某种事情来应对这种改变，比如结婚了想喊一嗓子，让所有人都恭喜我……
+
+在Javascript编程上，用到状态机的场合比比皆是。比如一个前端组件的状态，可能从‘隐藏’到‘显示’，从‘左边’到‘右边’，背景从‘白’到‘黑’，里面的图片从‘10’张到‘20张’等等……
+
+当组件从隐藏到显示的时候，你可能需要调用另一个组件来填充它的位置，这时候就需要在‘隐藏、显示’这个状态改变的时候做相应的操作。而促使这个组件改变可能有很多种情况（比如点击，或者过五秒隐藏），这时如果用状态机的话，只需要给它绑定一个状态改变事件而已……
+
+使用状态机，能让你的代码变得更直观、更整洁……
+
+当然，状态机能带来的好处远远不止这么多。
+
+## 如何使用？
+
+* 引入js
+
+        <script src="../state_machine.js"></script>
+
+* 初始化一个状态机
+
+        // 假设为一个人设置一个状态机 他有年龄、姓名、是否结婚、朋友数量、是否有房，是否有车等状态属性
+        var myState = new StateMachine({
+            // 年龄
+            age: 27,
+            // 姓名
+            name: 'season.chen',
+            // 是否结婚
+            marry: false,
+            // 朋友个数
+            friends: 10,
+            // 房
+            house: false,
+            // 车
+            car: false
+        });
+
+* 绑定事件：`myState.on(name, handler, conditions)` or `myState.on(handler, conditions)` or `myState.on(handler)`
+    
+    * `name`: 事件的名称，便于识别，也便于解除事件时使用
+
+    * `handler`: 事件处理程序   
+
+            handler = function(data) {
+                // data.from: 之前的状态
+                // data.to：之后的状态
+                // data.changes： 改变
+            }
+    
+    * `conditions`: 事件触发的条件，满足条件才会触发事件
+
+        如果condition是函数，它的返回值true,表示满足条件。如果condition是对象，对象里的所有条件都满足，才会触发事件。
+            
+            // 可以是函数
+            conditions = function(from, to, changes) {
+                // from: 之前的状态
+                // to：之后的状态
+                // changes： 改变
+            }
+
+            // 更多场合用对象
+            conditions = {
+                // 改变前的状态需满足
+                from: {
+                    age: 22, // 可以是字符串、数字、布尔值,
+                    friends: function(from, to) {return to > 20}, // 可以是函数
+                    name: ['name1', 'name2'], // 可以是数组,
+                    name: /name1|name2/, // 可以是正则,
+                },
+
+                // 改变后的状态需满足
+                to: {
+                    // 同from
+                },
+
+                // 这些状态需要改变
+                changes: [],
+
+                // 这些状态至少有一个改变
+                anyChanges: []
+            }
+
+* 移除事件：`myState.off(name, handler)` or `myState.off(handler)` or `myState.off(name)` or `myState.off()`
+
+* 将状态恢复到初始化状态: `myState.reset()`
+
+* 设置状态: `myState.set(stateKey, stateValue)` or `myState.set({name: '11', age: 1})`
+
+* 获取状态: `myState.get(stateKey)` or `myState.get()`
+
+* 触发事件： `myState.trigger(name, handler)` or `myState.trigger(name)` or `myState.trigger(handler)` or `myState.trigger()`
+
+### 例子(代码见`test/`目录)
+
+假设为一个人设置一个状态机
+
+    var myState = new StateMachine({
+        // 年龄
+        age: 27,
+        // 姓名
+        name: 'season.chen',
+        // 是否结婚
+        marry: false,
+        // 朋友个数
+        friends: 10,
+        // 房
+        house: false,
+        // 车
+        car: false
+    });
+
+年龄改变
+
+    myState.on('changeEge', function(data) {
+        console.log('哎呦，我又老了'+ (data.to.age - data.from.age) +'岁！');
+    }, {
+        change: ['age']
+    });
+    
+    myState.reset(); // 不是必须的，只是为了让例子更好理解，每次都恢复到最初状态
+    myState.set('age', 29);
+
+晚婚 大于30岁结婚
+
+    myState.on('lateMarriage', function(form, to) {
+        console.log('大于30岁才结婚，丢不丢人？');
+    }, {
+        from: {marry: false},
+        to: {
+            marry: true, 
+            age: function(from, to) {
+                return to >= 30;
+            }
+        }
+    }); 
+    
+    myState.reset();
+    myState.set({
+        marry: true,
+        age: 31
+    });
+
+早婚 小于30岁结婚
+
+    myState.on('earlyMarriage', function(form, to) {
+        console.log('不到30就结婚了，还可以额么么哒!');
+    }, {
+        from: {marry: false},
+        to: {
+            marry: true, 
+            age: function(from, to) {
+                return to < 30;
+            }
+        }
+    }); 
+    
+    myState.reset();
+    myState.set('marry', true);
+
+改了名字
+
+    myState.on('changeName', function(data) {
+        console.log('我将名字改成了' + data.to.name);
+    }, {
+        change: ['name']
+    });
+
+    myState.reset();
+    myState.set('name', '王大锤！');
+
+改了个奇葩名字
+
+    myState.on('changeWonderfulName', function(data) {
+        console.log('我是不是脑子坏掉了，竟然改了个这么挫的名字：' + data.to.name);
+    }, {
+        to: {
+            name: /王八蛋|傻子/
+        },
+        change: ['name']
+    });
+    
+    myState.reset();
+    myState.set('name', '王八蛋1');
+
+30岁前，买车、买房，还结了婚，我从此走向了人生巅峰！
+
+    myState.on(' toThePinnacle', function(data) {
+        console.log('哈哈哈！哈哈哈！我从此走向了人生巅峰！');
+    }, function(from, to, changes) {
+        // 年龄、车、房、结婚任何一个没有改变，不触发事件
+        if (!~changes.indexOf('age') 
+            && ~changes.indexOf('car') 
+            && ~changes.indexOf('house') 
+            && ~changes.indexOf('marry')) {
+            return false;
+        }
+
+        if (to.age < 30 && to.car && to.house && to.marry) {
+            return true;
+        }
+
+        return false;
+    });
+
+    myState.reset();
+    myState.set({
+        marry: true,
+        age: 28,
+        house: true,
+        car: true
+    });
--- state_machine.js
+++ state_machine.js
@@ -11,35 +11,44 @@ var StateMachine = function(states) {
         return;
     }
 
-    this._events = {
-        // 升职事件
-        'promote': [{
-            handler: function() {
-                console.log('我升职了！');
-            },
-            condition: {
-                from: {position: '普通职员'},
-                to: {position: '经理'},
-                where: {ege: 22}
-            }
-        }, {
-            handler: function() {
-                console.log('我升职了！');
-            },
-            condition: function(from, to) {
-                if (from.position != '经理' && to.position == '经理') {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }],
-        // 无名事件
-        'default': [{
-
-        }]
-    };
+    this._events = {};
     this._states = states;
+    this._initStates = this._cloneStates();
+}
+
+/**
+ * 将状态重置到初始化状态
+ */
+StateMachine.prototype.reset = function() {
+    this.set(this._initStates);
+}
+
+/**
+ * 设置状态
+ */
+StateMachine.prototype.set = function(stateKey, stateValue) {
+    var lasteStates = this._cloneStates();
+    var key;
+
+    if (typeof stateKey == 'object') {
+        for (key in stateKey) {
+            this._states[key] = stateKey[key];
+        }
+    }
+
+    this._states[stateKey] = stateValue;
+
+    // 是否触发事件
+    return this._check(lasteStates);
+}
+
+/**
+ * 获取当前状态
+ */
+StateMachine.prototype.get = function(stateKey) {
+    var curStates = this._cloneStates();
+    if (!arguments.length) return curStates;
+    return curStates(stateKey);
 }
 
 /**
@@ -100,14 +109,59 @@ StateMachine.prototype._offByHandler = function(handler) {
 }
 
 // 触发事件
-StateMachine.prototype.trigger = function() {
-    
+StateMachine.prototype.trigger = function(name, handler) {
+    // trigger all
+    if (arguments.length == 0) {
+        this._triggerByHandler('*');
+        return;
+    }
+
+    // .trigger(handler)
+    if (typeof name == 'function') {
+        this._triggerByHandler(name);
+        return;
+    }
+
+    if (!this._events[name]) return;
+
+    var curStates = this._cloneStates();
+    this._events[name].forEach(function(item) {
+        if (item.handler === handler) {
+            item.handler.call(that, {
+                name: name,
+                handler: item.handler,
+                from: curStates,
+                to: curStates
+            });
+        }
+    });
+}
+
+StateMachine.prototype._triggerByHandler = function(handler) {
+    var curStates = this._cloneStates();
+    var name;
+    var that = this;
+
+    for (name in this._events) {
+        this._events[name].forEach(function(item) {
+            if (handler == '*' || item.handler === handler) {
+                item.handler.call(that, {
+                    name: name,
+                    handler: item.handler,
+                    from: curStates,
+                    to: curStates
+                });
+            }
+        });
+    }
 }
 
 // 检查状态改变情况
 StateMachine.prototype._check = function(lastStates) {
     var changes = [];
+    var triggers = [];
     var name, key;
+    var that = this;
 
     for (key in this._states) {
         if (lastStates[key] != this._states[key]) {
@@ -115,42 +169,122 @@ StateMachine.prototype._check = function(lastStates) {
         }
     }
 
-    for (name in this._events) {
-        this._events[name].forEach(function(item) {
-            if (this._checkWhere(item, lastStates, changes)) {
+    // 状态没有发生改变，不会自动触发事件
+    if (!changes.length) return false;
 
+    for (name in that._events) {
+        that._events[name].forEach(function(item) {
+            var flag = false;
+
+            if (typeof item.conditions == 'function') {
+                flag = item.conditions(lastStates, that._cloneStates(), changes);
+            } else if ( that._checkFrom(item.conditions, lastStates, changes) 
+                && that._checkTo(item.conditions, that._states, changes) 
+                && that._change(item.conditions, that._states, changes) 
+                && that._anyChange(item.conditions, that._states, changes) ) {
+                flag = true;
             }
+            // save event to trigger
+            flag && triggers.push({
+                name: name,
+                handler: item.handler,
+                from: lastStates,
+                to: that._cloneStates()
+            });
         });        
     }
+
+    // 统一触发事件
+    triggers.forEach(function(item) {
+        item.handler.call(that, item);
+    });
+    
+    return true;
 }
 
-StateMachine.prototype._checkWhere = function(item, lastStates, changes) {
-    this._checkOneCondition(item['where'], lastStates, changes, lastStates);
+StateMachine.prototype._checkFrom = function(conditions, lastStates, changes) {
+    if (this._isEmptyObj(conditions)) return true;
+    return this._checkOneCondition(conditions['from'], lastStates, changes, lastStates);
 }
 
-StateMachine.prototype._checkFrom = function(item, lastStates, changes) {
-    this._checkOneCondition(item['form'], lastStates, changes, lastStates);
+StateMachine.prototype._checkTo = function(conditions, lastStates, changes) {
+    if (this._isEmptyObj(conditions)) return true;
+    return this._checkOneCondition(conditions['to'], this._states, changes, lastStates);
 }
 
-StateMachine.prototype._checkTo = function(item, lastStates, changes) {
-    this._checkOneCondition(item['to'], this._states, changes, lastStates);
+StateMachine.prototype._change = function(conditions, lastStates, changes) {
+    var flag = true;
+
+    if (this._isEmptyObj(conditions)) return true;
+    if (!Array.isArray(conditions.change)) return true;
+    conditions.change.forEach(function(v) {
+        if (!~changes.indexOf(v)) flag = false;
+    });
+    return flag;
 }
 
-StateMachine.prototype._checkOneCondition = function(condition, states, changes, lastStates) {
-    if (typeof condition == 'function') {
-        return condition(this._cloneState(), lastStates, changes);
-    }
+StateMachine.prototype._anyChange = function(conditions, lastStates, changes) {
+    var flag = false;
 
-    if (typeof this._isEmptyObj(condition)) return true;
+    if (this._isEmptyObj(conditions)) return true;
+    if (!Array.isArray(conditions.anyChange)) return true;
+    conditions.anyChange.forEach(function(v) {
+        if (~changes.indexOf(v)) flag = true;
+    });
+    return flag;
+}
+
+StateMachine.prototype._checkOneCondition = function(condition, compareStates, changes, lastStates) {
+    var curStates = this._cloneStates();
+
+    if (this._isEmptyObj(condition)) return true;
 
     var flag = true;
-    for (var key in item.where) {
-        if (!~changes.indexOf(key) || item.where[key] != lastStates[key]) {
-            flag = false;
-            break;
+    var key, cond, type;
+
+    for (key in condition) {
+        cond = condition[key];
+        type = this._getConditionType(cond);
+
+        switch (type) {
+            case 'function':
+                flag = cond(lastStates[key], curStates[key]);
+                break;
+            case 'string':
+            case 'other':
+                flag = compareStates[key] == cond;
+                break;
+            case 'array':
+                flag = ~cond.indexOf(compareStates[key]);
+                break;
+            case 'regexp':
+                flag = cond.test(compareStates[key]);
+                break;
+            default:
+                flag = false;
         }
+        if (!flag) break;
     }
     return flag;
+}
+
+// 判断条件的类型：function，string，array，regexp
+StateMachine.prototype._getConditionType = function(obj) {
+    var type = 'other';
+
+    if (typeof obj == 'function') {
+        type = 'function';
+    } else if (typeof  obj == 'string') {
+        type = 'string';
+    } else if (typeof obj == 'object') {
+        if (Array.isArray(obj)) {
+            type = 'array';
+        } else if (obj instanceof RegExp) {
+            type = 'regexp';
+        }
+    }
+
+    return type;
 }
 
 StateMachine.prototype._isEmptyObj = function(obj) {
@@ -160,11 +294,15 @@ StateMachine.prototype._isEmptyObj = function(obj) {
 }
 
 // 复制当前状态
-StateMachine.prototype._cloneState = function() {
+StateMachine.prototype._cloneStates = function(toCloneStates) {
     var states = {};
     var name;
+
+    toCloneStates = toCloneStates || this._states;
+
     for (name in this._states) {
         states[name] = this._states[name];
     }
+
     return states;
 }
